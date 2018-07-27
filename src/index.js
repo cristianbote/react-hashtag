9030bfe66f46b68006e498b2381cdf9e2cb0656f
Cristian Bote <botecristian@yahoo.com>
2018-07-27 09:53:39 +0300

    Moved all the logic inside one file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import { withFramework } from './hashtag';
+import { parse } from "../lib";
 
-const getParams = function() {
-    try {
-        return [require("react").createElement, true];
-    } catch (err) {
-        try {
-            return [require("preact").h];
-        } catch (err) {
-            console.log("[react-hashtag] there's no react nor preact available to import");
-        }
-    }
+let createElement;
+let canRenderArray = false;
+
+try {
+  createElement = require("react").createElement;
+  canRenderArray = true;
+} catch (err) {
+  try {
+    createElement = require("preact").h;
+    canRenderArray = false;
+  } catch (err) {
+    console.log(
+      "[react-hashtag] there's no react nor preact available to import"
+    );
+  }
+}
+
+const defaultHashtagRenderer = createElement => (hashtag, onClick) =>
+  createElement(
+    "span",
+    {
+      key: hashtag,
+      onClick: onClick ? e => onClick(hashtag, e) : null
+    },
+    hashtag
+  );
+
+export default props => {
+  const contents =
+    typeof props.children === "object" && props.children.length
+      ? !isNaN(props.children.length)
+        ? props.children[0]
+        : props.children
+      : props.children;
+  const hashtagRenderer =
+    props.renderHashtag || defaultHashtagRenderer(createElement);
+  const onHashtagClick = props.onHashtagClick;
+  const parsed = parse(contents, hashtagRenderer, onHashtagClick);
+
+  return canRenderArray ? parsed : createElement("span", null, parsed);
 };
-
-export default withFramework.apply(null, getParams());
